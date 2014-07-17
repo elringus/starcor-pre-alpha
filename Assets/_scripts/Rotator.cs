f17467c889e68aa30f3a35c699869fd3e9591ae9
Elringus <elringus@yandex.ru>
2014-07-17 12:16:53 +0400

    add planer

---
+++ Assets/_scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+	public Transform Transform;
+
+	public float XSpeed;
+	public float YSpeed;
+	public float ZSpeed;
+
+	private void Awake () 
+	{
+		Transform = transform;
+	}
+
+	private void Update () 
+	{
+		Transform.Rotate(new Vector3(XSpeed * Time.deltaTime, YSpeed * Time.deltaTime, ZSpeed * Time.deltaTime));
+	}
+}

c98170e6b120190296dd22e2a8813ea501891576
Elringus <elringus@yandex.ru>
2014-07-17 10:45:42 +0400

    initial commit

---
+++ Assets/Editor/AutoSave.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class AutoSave
+{
+	// auto save scene and assets on scene run
+	static AutoSave ()
+	{
+		EditorApplication.playmodeStateChanged = () =>
+		{
+			if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+			{
+				EditorApplication.SaveScene();
+				EditorApplication.SaveAssets();
+			}
+		};
+	}
+}

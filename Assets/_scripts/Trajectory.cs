ff415020e198bc51edea16174f16be796a77c34c
Elringus <elringus@yandex.ru>
2014-07-17 11:24:04 +0400

    add trajectory dummy

---
+++ Assets/_scripts/Trajectory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trajectory : MonoBehaviour
+{
+	#region SINGLETON
+	private static Trajectory _instance;
+	public static Trajectory I
+	{
+		get
+		{
+			if (_instance == null) _instance = FindObjectOfType(typeof(Trajectory)) as Trajectory;
+			return _instance;
+		}
+	}
+	private void OnApplicationQuit () { _instance = null; }
+	#endregion
+
+	private void Awake () 
+	{
+    	
+	}
+
+	private void Update () 
+	{
+    	
+	}
+}

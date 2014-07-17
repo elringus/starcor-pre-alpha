2c4ddfda6e9d3b501ec946347281be7593b394b0
Rupini <mstu_md@mail.ru>
2014-07-17 16:12:14 +0400

    Trajectory

--- Assets/_scripts/Trajectory.cs
+++ Assets/_scripts/Trajectory.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Trajectory : MonoBehaviour
 {
@@ -16,13 +18,77 @@ public class Trajectory : MonoBehaviour
 	private void OnApplicationQuit () { _instance = null; }
 	#endregion
 
+    public float MinDistance = 50;
+
+    private List<Vector3> Points { get; set; }
+    private LineRenderer lRenderer;
+    private int maxLRC = 200;
+    private int currLRC = 0;
+
 	private void Awake () 
 	{
-    	
+        Points = new List<Vector3>();
 	}
+
+    private void Start()
+    {
+        StartCoroutine(GenPoints());
+       
+        //Line Renderer
+        lRenderer = GetComponent<LineRenderer>();
+        lRenderer.enabled = false;
+        lRenderer.SetVertexCount(maxLRC);
+    }
 
 	private void Update () 
 	{
-    	
+        
 	}
+
+    private bool inConstruction = false;
+    private IEnumerator GenPoints()
+    {
+        while (true)
+        {
+
+            if (Input.GetMouseButton(0))
+            {
+
+                if (!inConstruction)
+                {
+                    Refresh();
+                    inConstruction = true;
+                }
+                var p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+                if (Points.Count < maxLRC)
+                    AddVertex(new Vector3(p.x, p.y, 0));
+            }
+
+            if (Input.GetMouseButtonUp(0))
+                inConstruction = false;
+
+            yield return new WaitForSeconds(0.002f);
+        }
+    }
+
+    private void AddVertex(Vector3 p)
+    {
+        if (Points.Count==0||Vector3.Distance(Points.Last(), p) > MinDistance)
+        {
+            Points.Add(p);
+            lRenderer.SetPosition(currLRC, p);
+            currLRC++;
+        }
+
+        for (int i = currLRC; i < maxLRC; i++)
+            lRenderer.SetPosition(i, p);
+    }
+   
+    private void Refresh()
+    {
+        Points.Clear();
+        lRenderer.enabled = true;
+        currLRC = 0;
+        //lRender.set
+    }
 }

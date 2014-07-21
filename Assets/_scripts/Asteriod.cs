694f5880d5d2e5d45824df2b71d5edeaf8c491cc
Elringus <elringus@yandex.ru>
2014-07-21 15:02:28 +0400

    add asteriods

---
+++ Assets/_scripts/Asteriod.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteriod : MonoBehaviour
+{
+	public Transform Transform;
+
+	private void Awake () 
+	{
+		Transform = transform;
+
+		float randomScale = Random.Range(.001f, .2f);
+		Transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+		Transform.eulerAngles = new Vector3(Random.Range(-180, 180), Random.Range(-180, 180), Random.Range(-180, 180));
+	}
+}
---
+++ Assets/_scripts/ObstacleThrower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleThrower : MonoBehaviour
+{
+	public float ThrowPower;
+
+	private void Awake () 
+	{
+    	
+	}
+
+	private void Update () 
+	{
+    	
+	}
+
+	public void OnCollisionEnter (Collision colli)
+	{
+		if (colli.collider.CompareTag("Obstacle"))
+			colli.rigidbody.AddForceAtPosition((colli.transform.position - transform.position).normalized * ThrowPower, colli.contacts[0].point, ForceMode.Impulse);
+	}
+}

c27ae69baced2df9c757258cde090eda52e65aed
Rupini <mstu_md@mail.ru>
2014-07-22 12:12:17 +0400

    exp fix

--- Assets/_scripts/ObstacleThrower.cs
+++ Assets/_scripts/ObstacleThrower.cs
@@ -22,7 +22,7 @@ public class ObstacleThrower : MonoBehaviour
             var colliders = Physics.OverlapSphere(transform.position, ExplosionRadius);
             foreach (var hit in colliders)
                 if (hit && hit.rigidbody)
-                    hit.rigidbody.AddExplosionForce(ThrowPower, transform.position, ExplosionRadius, 0.5f, ForceMode.Impulse);
+                    hit.rigidbody.AddExplosionForce(ThrowPower, transform.position, ExplosionRadius, 0.1f, ForceMode.Impulse);
         }
     }
 }

c7cdaeb3233b20334f81a8bffd9726a19f885011
Elringus <elringus@yandex.ru>
2014-07-22 12:06:37 +0400

    asteroid mass

--- Assets/_scripts/Asteriod.cs
+++ Assets/_scripts/Asteriod.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class Asteriod : MonoBehaviour
 {
 	public Transform Transform;
+	public Rigidbody Rigidbody;
 
 	private void Awake () 
 	{
 		Transform = transform;
+		Rigidbody = rigidbody;
 
 		float randomScale = Random.Range(.001f, .2f);
 		Transform.localScale = new Vector3(randomScale, randomScale, randomScale);
 		Transform.eulerAngles = new Vector3(Random.Range(-180, 180), Random.Range(-180, 180), Random.Range(-180, 180));
+		Rigidbody.mass = randomScale * 20;
 	}
 }

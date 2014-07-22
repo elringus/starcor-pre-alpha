6f1b1736648db994f56156eb6e736fefb4d90eaa
Rupini <mstu_md@mail.ru>
2014-07-22 11:27:14 +0400

    explosion

--- Assets/_scripts/ObstacleThrower.cs
+++ Assets/_scripts/ObstacleThrower.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ObstacleThrower : MonoBehaviour
 {
 	public float ThrowPower;
+    public float ExplosionRadius;
 
 	private void Awake () 
 	{
@@ -15,9 +16,14 @@ public class ObstacleThrower : MonoBehaviour
     	
 	}
 
-	public void OnCollisionEnter (Collision colli)
+	public void OnTriggerEnter (Collider col)
 	{
-		if (colli.collider.CompareTag("Obstacle"))
-			colli.rigidbody.AddForceAtPosition((colli.transform.position - transform.position).normalized * ThrowPower, colli.contacts[0].point, ForceMode.Impulse);
+        if (col.collider.CompareTag("Obstacle"))
+        {
+            var colliders = Physics.OverlapSphere(transform.position, ExplosionRadius);
+            foreach (var hit in colliders)
+                if (hit && hit.rigidbody)
+                    hit.rigidbody.AddExplosionForce(ThrowPower, transform.position, ExplosionRadius, 0.5f, ForceMode.Impulse);
+        }
 	}
 }

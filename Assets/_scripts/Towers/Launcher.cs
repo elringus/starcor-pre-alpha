a6b1a846bb8b94c082eba35502ccfe4576de6ae4
Rupini <mstu_md@mail.ru>
2014-07-23 20:34:26 +0400

    rounding :)

--- Assets/_scripts/Towers/Launcher.cs
+++ Assets/_scripts/Towers/Launcher.cs
@@ -15,8 +15,11 @@ public class Launcher : Tower
     protected LineRenderer lineRenderer;
     protected int maxVertCount = 200;
     protected int currVertCount = 0;
-
     protected Vector3 StartPoint;
+
+    private int stepsInSemicircle;
+    private float stepAngle;
+    private float interopStep;
     #endregion
     #region Base
 
@@ -26,8 +29,9 @@ public class Launcher : Tower
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.enabled = false;
         lineRenderer.SetVertexCount(maxVertCount);
+        interopStep = StepLength / InteropFactor;
         StartPoint = Transform.position;
-        IntialCorrect();
+        IntialCorrectValues();
     }
     protected override void StartTargeting()
     {
@@ -44,15 +48,14 @@ public class Launcher : Tower
     }
     protected override void FinishTargeting()
     {
-        if (Points.Count < 2)
-            lineRenderer.enabled = false;
-        else
+        if (Points.Count > 1)
         {
             for (int i = currVertCount; i < maxVertCount; i++)
                 lineRenderer.SetPosition(i, Points.Last());
             Produce();
         }
 
+        lineRenderer.enabled = false;
         InProcess = false;
     }
 
@@ -69,29 +72,32 @@ public class Launcher : Tower
         var rocky = ((GameObject)Instantiate(Prototype, Points[0], Quaternion.identity));
         rocky.GetComponent<Rocket>().Initialize(GetInteropPoints(Points));
     }
-    
+
     #endregion
     #region Methods
     protected List<Vector3> GetInteropPoints(List<Vector3> points)
     {
         int iCount;
-        float iStep = StepLength / InteropFactor; //length of intrepolation step
         List<Vector3> iPoinst = new List<Vector3>();
         for (int i = 0; i < points.Count - 1; i++)
         {
-            iCount = Mathf.CeilToInt(Vector3.Distance(points[i], points[i + 1]) / iStep);
+            iCount = Mathf.CeilToInt(Vector3.Distance(points[i], points[i + 1]) / interopStep);
             for (int j = 0; j < iCount - 1; j++)
-                iPoinst.Add((points[i + 1] - points[i]).normalized * iStep * j + points[i]);
+                iPoinst.Add((points[i + 1] - points[i]).normalized * interopStep * j + points[i]);
         }
 
         return iPoinst;
     }
+
     protected void AddVertex(Vector3 p)
     {
         if (Points.Count == 0 || Vector3.Distance(Points.Last(), p) >= StepLength)
         {
-            if (Correct(p))
+            if (CheckCorrect(p))
             {
+                if (Points.Count > 0)
+                    p = (p - Points.Last()).normalized * StepLength + Points.Last();
+
                 Points.Add(p);
                 lineRenderer.SetPosition(currVertCount, p);
                 currVertCount++;
@@ -101,6 +107,12 @@ public class Launcher : Tower
         for (int i = currVertCount; i < maxVertCount; i++)
             lineRenderer.SetPosition(i, p);
     }
+
+    protected List<Vector3> GetInteropPoints(Vector3 p1, Vector3 p2)
+    {
+        return GetInteropPoints((new Vector3[] { p1, p2 }).ToList());
+    }
+
     protected void AddDeathPath()
     {
         var v = (Points[Points.Count - 1] - Points[Points.Count - 2]).normalized;
@@ -109,25 +121,32 @@ public class Launcher : Tower
             Points.Add(v * StepLength * i + lastpoint);
     }
 
-    private int stepCount;
-    private float stepAngle;
-    private void IntialCorrect()
+    private void IntialCorrectValues()
     {
-        stepCount = Mathf.CeilToInt(Mathf.PI / (Mathf.Asin(StepLength / 2 * MinRadius)) / 2);
-        stepAngle = Mathf.PI / (2 * stepCount);
+        stepsInSemicircle = Mathf.CeilToInt(Mathf.PI / (Mathf.Asin(StepLength / (2 * MinRadius))) / 2);
+        stepAngle = Mathf.PI / (2 * stepsInSemicircle);
+        Debug.Log(stepsInSemicircle);
     }
 
-    protected bool Correct(Vector3 p)
+    protected bool CheckCorrect(Vector3 p)
     {
         if (Points.Count < 2)
             return true;
-        int checkVert = Points.Count - stepCount;
-        int n=2;
+
+        int checkVert = Points.Count - stepsInSemicircle;
+        int n = 2;
         if (checkVert < 0)
             checkVert = 0;
+
+        var nextStep = (p - Points.Last()).normalized * StepLength + Points.Last();
+        //var angle = Mathf.Acos(Vector3.Dot(p - Points.Last(), Points[Points.Count - 1] - Points[Points.Count - 2]));
+        //Debug.Log(angle*Mathf.Rad2Deg+" "+minAngle);
+        //if (Mathf.Abs(angle) < minAngle)
+        //    return false;
+
         for (int i = Points.Count - 2; i >= checkVert; i--)
         {
-            if (Vector3.Distance(p, Points[i]) < 2 * MinRadius * Mathf.Sin(stepAngle * n))
+            if (Vector3.Distance(nextStep, Points[i]) < 2 * MinRadius * Mathf.Sin(stepAngle * n))
                 return false;
             n++;
         }

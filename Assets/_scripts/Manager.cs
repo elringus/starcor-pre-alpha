3026c670d8470c92df362cb4646650f63093524c
Elringus <elringus@yandex.ru>
2014-07-24 16:45:10 +0400

    touch input

--- Assets/_scripts/Manager.cs
+++ Assets/_scripts/Manager.cs
@@ -42,10 +42,21 @@ public class Manager : MonoBehaviour
 		{
 			if (SelectedTower && StartedTargetting)
 			{
-				if (Input.GetMouseButtonDown(0)) SelectedTower.Targeting(TargetingType.Start);
-				if (Input.GetMouseButton(0)) SelectedTower.Targeting(TargetingType.InProcess);
-				else if (Input.GetMouseButtonUp(0)) { SelectedTower.Targeting(TargetingType.Finish); StartedTargetting = false; }
-				else SelectedTower.Targeting(TargetingType.None);
+				if (Application.isEditor)
+				{
+					if (Input.GetMouseButtonDown(0)) SelectedTower.Targeting(TargetingType.Start);
+					if (Input.GetMouseButton(0)) SelectedTower.Targeting(TargetingType.InProcess);
+					else if (Input.GetMouseButtonUp(0)) { SelectedTower.Targeting(TargetingType.Finish); StartedTargetting = false; }
+					else SelectedTower.Targeting(TargetingType.None);
+				}
+				else
+				{
+					Touch touch = Input.GetTouch(0);
+					if (touch.phase == TouchPhase.Began) SelectedTower.Targeting(TargetingType.Start);
+					else if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) SelectedTower.Targeting(TargetingType.InProcess);
+					else if (touch.phase == TouchPhase.Ended) { SelectedTower.Targeting(TargetingType.Finish); StartedTargetting = false; }
+					else SelectedTower.Targeting(TargetingType.None);
+				}
 			}
 			yield return null;
 		}

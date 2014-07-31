51005e020e4be83df8aca89fb4933cb718f1349a
Elringus <elringus@yandex.ru>
2014-07-31 15:57:53 +0400

    increase death distnce

--- Assets/_scripts/Rocket.cs
+++ Assets/_scripts/Rocket.cs
@@ -69,8 +69,7 @@ public class Rocket : MonoBehaviour
                 lastIndex = currIndex;
             }
         }
-        else
-            Destroy(GameObject);
+        else Destroy(GameObject);
         
     }
 
